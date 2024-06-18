ca424b20613327676f678904847258aafe8ac9ff
Artem Lubentsov <artem.law@mail.ru>
2024-06-18 20:21:46 +0300

    Fix utils

--- api/utils.py
+++ api/utils.py
@@ -1,9 +1,7 @@
 import os
-from dotenv import load_dotenv
 
 
 def get_api_tokens():
-    # Проверяем, выполняется ли код в Google Colab
     try:
         from google.colab import userdata
         MS_API_TOKEN = userdata.get('MS_API_TOKEN')
@@ -12,7 +10,6 @@ def get_api_tokens():
     except ImportError:
         pass
 
-    load_dotenv()
     MS_API_TOKEN = os.getenv('MS_API_TOKEN')
     WB_API_TOKEN = os.getenv('WB_API_TOKEN')
 
