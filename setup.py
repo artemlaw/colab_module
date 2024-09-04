e2fdc8fe5f4998571f5527ccedac415d1ac2caa3
Artem Lubentsov <artem.law@mail.ru>
2024-09-05 01:19:10 +0300

    Update module version: 0.0.2 -> 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='colab_module',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     install_requires=['requests', 'pymupdf', 'reportlab'],
     extras_require={
